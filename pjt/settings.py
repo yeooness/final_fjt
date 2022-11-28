ee2cefc82a74b9e1630beb3e8b4eb66ceb4f5044
lsb0709 <dltkdqor0709@gmail.com>
2022-11-28 16:42:03 +0900

    community

--- pjt/settings.py
+++ pjt/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = 'django-insecure-u75c@yyzhv059zm$c8@vm!ct=2eo1$)_dmawn#9%=8b4q-00j9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG") == "True"
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
