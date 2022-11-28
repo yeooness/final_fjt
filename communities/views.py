a07c6c069aa36a66e4c2c21c6f5bda1d4119284d
황여원 <yeooness@naver.com>
2022-11-28 17:53:08 +0900

    dogwalking crud 수정중

--- communities/views.py
+++ communities/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from .models import Community
 from .forms import CommunityForm
+
 # Create your views here.
 # 기본 crud 글쓰기를 원버튼으로 통일 글 전체목록이 됨
+
 
 def index(request):
     communities = Community.objects.order_by("-pk")
@@ -14,6 +16,7 @@ def index(request):
     }
     return render(request, "communities/index.html", context)
 
+
 def create(request):
     if request.method == "POST":
         community_form = CommunityForm(request.POST, request.FILES)
@@ -24,7 +27,10 @@ def create(request):
             return redirect("communities:index")
     else:
         community_form = CommunityForm()
-    return render(request, "communities/create.html", { "community_form": community_form })
+    return render(
+        request, "communities/create.html", {"community_form": community_form}
+    )
+
 
 def detail(request, community_pk):
     community = Community.objects.get(pk=community_pk)
@@ -33,22 +39,27 @@ def detail(request, community_pk):
     }
     return render(request, "communities/detail.html", context)
 
+
 def update(request, community_pk):
     community = Community.objects.get(pk=community_pk)
 
     if request.user == community.user:
         if request.method == "POST":
-            community_form = CommunityForm(request.POST, request.FILES, instance=community)
+            community_form = CommunityForm(
+                request.POST, request.FILES, instance=community
+            )
 
             if community_form.is_valid():
                 community_form.save()
                 return redirect("communities:detail", community_pk)
         else:
             community_form = CommunityForm(instance=community)
-        return render(request, "communities/update.html", {"community_form": community_form})
+        return render(
+            request, "communities/update.html", {"community_form": community_form}
+        )
+
 
 def delete(request, community_pk):
     Community.objects.get(pk=community_pk).delete()
 
     return redirect("communities:index")
-
