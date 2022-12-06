7b7a24a2be3cbb694f0a3d3a588526c037412bfe
황여원 <yeooness@naver.com>
2022-12-06 09:22:31 +0900

    달력 일지 작성

---
+++ calendarapp/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from . import views
+
+app_name = "calendarapp"
+
+urlpatterns = [
+    path("index", views.index, name="index"),
+    path("", views.CalendarView.as_view(), name="calendar"),
+    path("event/new/", views.create_event, name="event_new"),
+    path("event/edit/<int:pk>/", views.EventEdit.as_view(), name="event_edit"),
+    path("event/<int:event_id>/details/", views.event_details, name="event-detail"),
+    path(
+        "add_eventmember/<int:event_id>", views.add_eventmember, name="add_eventmember"
+    ),
+    path(
+        "event/<int:pk>/remove",
+        views.EventMemberDeleteView.as_view(),
+        name="remove_event",
+    ),
+    # 산책일지
+    path("dogwalking/new/", views.create_dogwalking, name="dogwalking_new"),
+]
