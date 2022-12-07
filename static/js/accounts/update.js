70f2432c8c275d357af623b1f9ab696c09f1c2e9
황여원 <yeooness@naver.com>
2022-12-07 21:53:39 +0900

    지도수정

--- static/js/accounts/update.js
+++ static/js/accounts/update.js
@@ -28,8 +28,8 @@ function sample6_execDaumPostcode() {
           if (data.buildingName !== '' && data.apartment === 'Y') {
             extraAddr += (
               extraAddr !== ''
-              ? ', ' + data.buildingName
-              : data.buildingName);
+                ? ', ' + data.buildingName
+                : data.buildingName);
           }
           // 표시할 참고항목이 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
           if (extraAddr !== '') {
@@ -89,67 +89,67 @@ getAuthBtn.addEventListener("click", (event) => {
       },
       data: formData
     })
-    // 일일 허용 횟수 검사
+      // 일일 허용 횟수 검사
       .then(response => {
-      if (response.data.authCount == 5) {
-        phoneId.setAttribute("disabled", true);
-        getAuthBtn.setAttribute("disabled", true);
-        helpText
-          .classList
-          .remove("d-none");
-        helpText.textContent = "오늘은 더 이상 인증이 불가능합니다. (최대 인증횟수 5회)";
-      } else {
-        // 인증번호 입력란 보여주기
-        if (document.querySelector("#already-auth-user") != null) {
-          const phoneDiv = document.getElementById("phone-div");
-          const alreadyAuthUser = document.querySelector("#already-auth-user");
-          phoneDiv.removeChild(alreadyAuthUser);
+        if (response.data.authCount == 5) {
+          phoneId.setAttribute("disabled", true);
+          getAuthBtn.setAttribute("disabled", true);
+          helpText
+            .classList
+            .remove("d-none");
+          helpText.textContent = "오늘은 더 이상 인증이 불가능합니다. (최대 인증횟수 5회)";
+        } else {
+          // 인증번호 입력란 보여주기
+          if (document.querySelector("#already-auth-user") != null) {
+            const phoneDiv = document.getElementById("phone-div");
+            const alreadyAuthUser = document.querySelector("#already-auth-user");
+            phoneDiv.removeChild(alreadyAuthUser);
+          };
+          getAuthBtn
+            .classList
+            .add("d-none");
+          authPhone
+            .classList
+            .remove("d-none");
+          authPhoneSubmit
+            .classList
+            .remove("d-none");
+          authTimer
+            .classList
+            .remove("d-none");
+          helpText
+            .classList
+            .remove("d-none");
+          let formData = new FormData();
+          formData.append("phone", phoneId.value);
+          axios({
+            method: 'post',
+            url: `/accounts/${event.target.dataset.accountId}/update/phone-auth/`,
+            headers: {
+              'X-CSRFToken': csrftoken
+            },
+            data: formData
+          }).then(response => {
+            let totalSec = 300;
+            let min = '';
+            let sec = '';
+            let authInterval = setInterval(function () {
+              let min = parseInt(totalSec / 60);
+              let sec = totalSec % 60;
+              if (sec < 10) {
+                authTimer.textContent = min + " : 0" + sec + "\u00a0";
+              } else {
+                authTimer.textContent = min + " : " + sec + "\u00a0";
+              }
+              totalSec--;
+              if (totalSec < 0) {
+                clearInterval(authInterval);
+                authTimer.textContent = "인증 시간이 만료되었습니다.";
+              };
+            }, 1000);
+          });
         };
-        getAuthBtn
-          .classList
-          .add("d-none");
-        authPhone
-          .classList
-          .remove("d-none");
-        authPhoneSubmit
-          .classList
-          .remove("d-none");
-        authTimer
-          .classList
-          .remove("d-none");
-        helpText
-          .classList
-          .remove("d-none");
-        let formData = new FormData();
-        formData.append("phone", phoneId.value);
-        axios({
-          method: 'post',
-          url: `/accounts/${event.target.dataset.accountId}/update/phone-auth/`,
-          headers: {
-            'X-CSRFToken': csrftoken
-          },
-          data: formData
-        }).then(response => {
-          let totalSec = 300;
-          let min = '';
-          let sec = '';
-          let authInterval = setInterval(function () {
-            let min = parseInt(totalSec / 60);
-            let sec = totalSec % 60;
-            if (sec < 10) {
-              authTimer.textContent = min + " : 0" + sec + "\u00a0";
-            } else {
-              authTimer.textContent = min + " : " + sec + "\u00a0";
-            }
-            totalSec--;
-            if (totalSec < 0) {
-              clearInterval(authInterval);
-              authTimer.textContent = "인증 시간이 만료되었습니다.";
-            };
-          }, 1000);
-        });
-      };
-    });
+      });
   } else {
     phoneId.focus();
   };
