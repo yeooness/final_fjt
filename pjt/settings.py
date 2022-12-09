907fac37a6b610ba7a980387767260bdf81efcdf
han-gaeul <gmlakdcjs314@gmail.com>
2022-12-09 15:35:09 +0900

    static 수정

--- pjt/settings.py
+++ pjt/settings.py
@@ -142,8 +142,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = "/static/"
-STATIC_ROOT = "staticfiles"
+# STATIC_URL = "/static/"
+# STATIC_ROOT = "staticfiles"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
