6d11f8a753f8af98663e489ee3e2d9baa1f3402c
han-gaeul <gmlakdcjs314@gmail.com>
2022-12-12 22:29:38 +0900

    일기 작성 - 수정 해야함

--- journal/models.py
+++ journal/models.py
@@ -8,34 +8,23 @@ from django.utils import timezone
 # Create your models here.
 
 
-class DogwalkingJournal(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    route = models.TextField()
-    consumed_calories = models.IntegerField()
-    walking_time = models.IntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    # 작성 시간
-    @property
-    def created_string(self):
-        time = datetime.now(tz=timezone.utc) - self.created_at
-
-        if time < timedelta(minutes=1):
-            return "방금 전"
-        elif time < timedelta(hours=1):
-            return str(int(time.seconds / 60)) + "분 전"
-        elif time < timedelta(days=1):
-            return str(int(time.seconds / 3600)) + "시간 전"
-        # elif time < timedelta(days=2):
-        #     time = datetime.now(tz=timezone.utc).date() - self.created_at.date()
-        #     return str(time.days) + '일 전'
-        else:
-            return False
-
-
-class DailyJournal(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    content = models.TextField()
+class Journal(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    pet = models.ForeignKey(Pet, on_delete=models.CASCADE)
+    journal_board_choices = [
+        ("일기", "일기"),
+        ("산책일기", "산책일기"),
+        ("건강일기", "건강일기"),
+    ]
+    journal_board = models.CharField(
+        max_length=20, choices=journal_board_choices, default="선택"
+    )
+    # 산책
+    route = models.TextField(null=True)
+    consumed_calories = models.IntegerField(null=True)
+    walking_time = models.IntegerField(null=True)
+    # 데일리
+    content = models.TextField(null=True)
     image = ProcessedImageField(
         upload_to="images/daily_journal/",
         blank=True,
@@ -43,35 +32,14 @@ class DailyJournal(models.Model):
         format="JPEG",
         options={"quality": 80},
     )
+    # 건강
+    meals = models.CharField(max_length=100, null=True)
+    energy = models.CharField(max_length=100, null=True)
+    medicine = models.CharField(max_length=100, null=True)
+    # 작성시간
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    # 작성 시간
-    @property
-    def created_string(self):
-        time = datetime.now(tz=timezone.utc) - self.created_at
 
-        if time < timedelta(minutes=1):
-            return "방금 전"
-        elif time < timedelta(hours=1):
-            return str(int(time.seconds / 60)) + "분 전"
-        elif time < timedelta(days=1):
-            return str(int(time.seconds / 3600)) + "시간 전"
-        # elif time < timedelta(days=2):
-        #     time = datetime.now(tz=timezone.utc).date() - self.created_at.date()
-        #     return str(time.days) + '일 전'
-        else:
-            return False
-
-
-class HealthJournal(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    meals = models.CharField(max_length=100)
-    energy = models.CharField(max_length=100)
-    medicine = models.CharField(max_length=100)
-    # time = models.DateTimeField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    # 작성 시간
     @property
     def created_string(self):
         time = datetime.now(tz=timezone.utc) - self.created_at
