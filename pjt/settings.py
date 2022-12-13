630ad03c899933bb9d4f2ab11aae8aa08faff8bc
han-gaeul <gmlakdcjs314@gmail.com>
2022-12-13 11:30:30 +0900

    배포 수정

--- pjt/settings.py
+++ pjt/settings.py
@@ -31,7 +31,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
-    "Danggeunjibsabean-env-1.eba-3ncw627q.ap-northeast-2.elasticbeanstalk.com",
+    "Danggeunjibsa-env.eba-y8yce3qe.ap-northeast-2.elasticbeanstalk.com",
 ]
 
 
